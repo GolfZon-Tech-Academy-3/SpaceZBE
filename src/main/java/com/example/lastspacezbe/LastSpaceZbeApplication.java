4cbc7f1542b87e62c236c3ec9ccd6e770fc8b770
hmhmchm <hyeonminchoi5@gmail.com>
2022-11-15 00:24:06 +0900

    project create(add postgres)

---
+++ src/main/java/com/example/lastspacezbe/LastSpaceZbeApplication.java
@@ -0,0 +1,13 @@
+package com.example.lastspacezbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LastSpaceZbeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LastSpaceZbeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lastspacezbe/LastSpaceZbeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lastspacezbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LastSpaceZbeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
