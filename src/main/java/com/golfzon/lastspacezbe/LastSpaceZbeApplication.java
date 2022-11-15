abb84a630f9a205baaaf8a182c3092a1651c2ca9
journeypark93 <wwwsomty@gmail.com>
2022-11-15 10:51:50 +0900

    feat : Change package name from 'example' to 'golfzon'

--- src/main/java/com/golfzon/lastspacezbe/LastSpaceZbeApplication.java
+++ src/main/java/com/golfzon/lastspacezbe/LastSpaceZbeApplication.java
@@ -1,4 +1,4 @@
-package com.example.lastspacezbe;
+package com.golfzon.lastspacezbe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/golfzon/lastspacezbe/LastSpaceZbeApplicationTests.java
+++ src/test/java/com/golfzon/lastspacezbe/LastSpaceZbeApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.lastspacezbe;
+package com.golfzon.lastspacezbe;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
