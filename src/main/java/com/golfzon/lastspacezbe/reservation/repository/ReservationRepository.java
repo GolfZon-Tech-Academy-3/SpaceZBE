f53ff1e73baa20c822b741e9e8175a60cd54ef82
hmhmchm <hyeonminchoi5@gmail.com>
2022-11-17 16:29:57 +0900

    feat: 시간에 따른 예약 취소(type : desk, meeting room)

--- src/main/java/com/golfzon/lastspacezbe/reservation/repository/ReservationRepository.java
+++ src/main/java/com/golfzon/lastspacezbe/reservation/repository/ReservationRepository.java
@@ -2,11 +2,17 @@ package com.golfzon.lastspacezbe.reservation.repository;
 
 import com.golfzon.lastspacezbe.reservation.entity.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 
 public interface ReservationRepository extends JpaRepository<Reservation,Long> {
 
     // 예약취소
     Reservation findAllByReservationId(Long reservationId);
+    // 예약한 시간
+    Reservation findByReservationId(Long reservationId);
+
+//    @Query(value = "SELECT r (TO_DATE(TO_CHAR(NOW(), 'YYYY-MM-DD HH24:MI'),'YYYY-MM-DD HH24:MI') - TO_DATE(r.reserveTime,'YYYY-MM-DD HH24:MI'))*24 from Reservation r where r.reservationId=?1")
+//    float findByOneTime(Long reservationId);
 //    List<TroubleComment> findAllByNickname(String nickname);
 }
--- src/main/java/com/golfzon/lastspacezbe/reservation/service/ReservationService.java
+++ src/main/java/com/golfzon/lastspacezbe/reservation/service/ReservationService.java
@@ -4,8 +4,13 @@ import com.golfzon.lastspacezbe.reservation.dto.ReservationRequestDto;
 import com.golfzon.lastspacezbe.reservation.entity.Reservation;
 import com.golfzon.lastspacezbe.reservation.repository.ReservationRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class ReservationService {
@@ -23,11 +28,39 @@ public class ReservationService {
         reservationRepository.save(reservation);
     }
 
-    // 예약 취소하기
-    public void cancel(Long reservationId){
+    // 오피스 예약 취소하기
+    public void officeCancel(Long reservationId){
 
         Reservation reservation = reservationRepository.findAllByReservationId(reservationId);
         reservation.setStatus("002"); // 예약상태를 예약 취소로 바꾸기
+        reservationRepository.save(reservation); // 002 예약상태 저장
+
+    }
+    // 데스크/회의실 예약 취소하기
+    public void deskCancel(Long reservationId){
+
+        Reservation reservation = reservationRepository.findAllByReservationId(reservationId);
+        reservation.setStatus("002"); // 예약상태를 예약 취소로 바꾸기
+        // 예약한 시간
+        LocalDateTime reserveTime = reservationRepository.findByReservationId(reservationId).getReserveTime();
+        // 현재 시간
+        LocalDateTime currentDateTime = LocalDateTime.now();
+        // 예약한 시간 현재시간 차이
+        Duration duration = Duration.between(reserveTime, currentDateTime);
+
+        log.info("reserveTime : {}",reserveTime);
+        log.info("currentTime : {}",currentDateTime);
+        log.info("Time check : {}초",duration.getSeconds());
+
+        // 예약한지 1시간 이내
+        if(duration.getSeconds() <= 3600){
+
+        }
+        // 예약한지 1시간 이후
+        else if(duration.getSeconds() > 3600){
+
+        }
+
         reservationRepository.save(reservation); // 002 예약상태 저장
 
     }
