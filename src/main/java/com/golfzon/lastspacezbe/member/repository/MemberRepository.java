aff315e6fbc26893da7e03e5bf0d12b0579629f0
hmhmchm <hyeonminchoi5@gmail.com>
2022-11-20 16:52:54 +0900

    feat : 사무공간 등록/ 수정 기능

--- src/main/java/com/golfzon/lastspacezbe/member/repository/MemberRepository.java
+++ src/main/java/com/golfzon/lastspacezbe/member/repository/MemberRepository.java
@@ -1,12 +1,17 @@
 package com.golfzon.lastspacezbe.member.repository;
 
+import com.golfzon.lastspacezbe.company.entity.Company;
 import com.golfzon.lastspacezbe.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member,Long> {
     Optional<Member> findByEmail(String email);
 
     Optional<Member> findByMemberName(String memberName);
+
+    // memberId 로 Company 업체 조회
+    Optional<Company> findByMemberId(Long memberId);
 }
