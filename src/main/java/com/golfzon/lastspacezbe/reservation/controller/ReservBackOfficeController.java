59ef1417724bdc59f887e6214a9174ac3b315842
hmhmchm <hyeonminchoi5@gmail.com>
2022-11-21 14:13:00 +0900

    feat : Create Company reservation status

--- src/main/java/com/golfzon/lastspacezbe/reservation/controller/ReservBackOfficeController.java
+++ src/main/java/com/golfzon/lastspacezbe/reservation/controller/ReservBackOfficeController.java
@@ -1,12 +1,16 @@
 package com.golfzon.lastspacezbe.reservation.controller;
 
 import com.golfzon.lastspacezbe.reservation.dto.ReservationRequestDto;
+import com.golfzon.lastspacezbe.reservation.dto.ReservationResponseDto;
+import com.golfzon.lastspacezbe.reservation.service.ReservBackOfficeService;
 import com.golfzon.lastspacezbe.reservation.service.ReservationService;
 import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -15,13 +19,14 @@ import org.springframework.web.bind.annotation.*;
 public class ReservBackOfficeController {
 
     private final ReservationService reservationService;
+    private final ReservBackOfficeService reservBackOfficeService;
 
     // 오늘 업체 예약 수
     @GetMapping("/reservation/count/{companyId}")
     public ResponseEntity<Integer> todayReserve(@PathVariable(name="companyId") Long companyId) {
 
         // 예약 수
-        int reserveCount = reservationService.todayReserve(companyId);
+        int reserveCount = reservBackOfficeService.todayReserve(companyId);
 
         return ResponseEntity.ok()
                 .body(reserveCount);
@@ -32,9 +37,19 @@ public class ReservBackOfficeController {
     public ResponseEntity<Integer> todayCancel(@PathVariable(name="companyId") Long companyId) {
 
         // 예약 취소 수
-        int cancelCount = reservationService.todayCancel(companyId);
+        int cancelCount = reservBackOfficeService.todayCancel(companyId);
 
         return ResponseEntity.ok()
                 .body(cancelCount);
+    }
+
+    // 예약 현황
+    @GetMapping("/reservation/total/{companyId}")
+    public ResponseEntity<List<ReservationResponseDto>> totalReservation(@PathVariable(name="companyId") Long companyId) {
+
+        List<ReservationResponseDto> reservationResponseDtos = reservBackOfficeService.totalReserveSelectAll(companyId);
+
+        return ResponseEntity.ok()
+                .body(reservationResponseDtos);
     }
 }
