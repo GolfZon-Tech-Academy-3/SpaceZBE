61333081d84d2a970f08583f5e9c87844f8eea79
journeypark93 <wwwsomty@gmail.com>
2022-11-28 16:51:18 +0900

    feat : Searching company(total, desk, office, meeting room)

--- src/main/java/com/golfzon/lastspacezbe/space/repository/SpaceRepository.java
+++ src/main/java/com/golfzon/lastspacezbe/space/repository/SpaceRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Set;
 
 
 public interface SpaceRepository extends JpaRepository<Space,Long> {
@@ -13,21 +14,40 @@ public interface SpaceRepository extends JpaRepository<Space,Long> {
 
     Space findBySpaceId(Long spaceId);
 
+    // 오피스로 등록된 공간 모두 조회
+    @Query
+            (value = "select distinct company_id from space where type like '오피스'",
+            nativeQuery = true)
+    Set<Long> findAllCompanyIdByOffice();
+    // 오피스로 등록된 공간 등록된 순으로 9개씩 조회
     @Query
             (value = "select distinct company_id from space where type like '오피스' order by company_id desc",
             countQuery = "select distinct company_id from space where type like '오피스' order by company_id desc",
             nativeQuery = true)
-    List<Long> findAllByOfficeOrderByCompanyIdDesc(Pageable pageable);
+    List<Long> findAllOfficeCompany(Pageable pageable);
 
+    // 데스크로 등록된 공간 모두 조회
+    @Query
+            (value = "select distinct company_id from space where type like '데스크'",
+                    nativeQuery = true)
+    Set<Long> findAllCompanyIdByDesk();
+    // 데스크로 등록된 공간 등록된 순으로 9개씩 조회
     @Query
             (value = "select distinct company_id from space where type like '데스크' order by company_id desc",
                     countQuery = "select distinct company_id from space where type like '데스크' order by company_id desc",
                     nativeQuery = true)
     List<Long> findAllByDeskOrderByCompanyIdDesc(Pageable pageable);
 
+    // 회의실로 등록된 공간 모두 조회
+    @Query
+            (value = "select distinct company_id from space where type like '%회의실%'",
+                    nativeQuery = true)
+    Set<Long> findAllCompanyIdByMeetingRoom();
+    // 회의실로 등록된 공간 등록된 순으로 9개씩 조회
     @Query
             (value = "select distinct company_id from space where type like '%회의실%' order by company_id desc",
                     countQuery = "select distinct company_id from space where type like '%회의실%' order by company_id desc",
                     nativeQuery = true)
     List<Long> findAllByMeetingRoomOrderByCompanyIdDesc(Pageable pageable);
+
 }
