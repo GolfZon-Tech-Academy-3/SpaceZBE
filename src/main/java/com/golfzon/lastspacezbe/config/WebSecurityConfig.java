0b37f2c254da8e5d75710934a8a355c471ff4fec
journeypark93 <wwwsomty@gmail.com>
2022-12-03 15:17:10 +0900

    fix : Accept cerboot's request

--- src/main/java/com/golfzon/lastspacezbe/config/WebSecurityConfig.java
+++ src/main/java/com/golfzon/lastspacezbe/config/WebSecurityConfig.java
@@ -187,12 +187,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        //configuration.addAllowedOrigin("http://localhost:3000"); // local 테스트 시
-        //configuration.addAllowedOrigin("54.180.201.231"); // local 테스트 시
-        //configuration.addAllowedOrigin("http://localhost:8080"); // local 테스트 시
-        //configuration.addAllowedOrigin("http://localhost:8081"); // local 테스트 시
+        configuration.addAllowedOrigin("http://localhost:3000"); // local 테스트 시
+        configuration.addAllowedOrigin("https://acme-v02.api.letsencrypt.org:443"); // local 테스트 시
+        configuration.addAllowedOrigin("https://acme-v02.api.letsencrypt.org"); // local 테스트 시
+        configuration.addAllowedOrigin("http://localhost:8080"); // local 테스트 시
+        configuration.addAllowedOrigin("http://localhost:8081"); // local 테스트 시
         configuration.addAllowedOrigin("*"); // local 테스트 시
-        //configuration.addAllowedOrigin("https://main.d3ezz3muzp1rz5.amplifyapp.com"); // 배포 시
         configuration.addAllowedOriginPattern("*");
         configuration.addAllowedMethod("*");
         configuration.addAllowedHeader("*");
